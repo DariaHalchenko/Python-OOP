2ef29fae24956d64f40f36ad804f6c103c3fca10
DariaHalchenko <154507353+DariaHalchenko@users.noreply.github.com>
2024-05-31 10:25:46 +0300

    Create Ulesanne:-Twitter.py

---
+++ Ulesanne:-Twitter.py
@@ -0,0 +1,127 @@
+"""Twitter."""
+
+
+class Tweet:
+    """Tweet class."""
+
+    def __init__(self, user: str, content: str, time: float, retweets: int):
+        """
+        Tweet constructor.
+
+        :param user: Author of the tweet.
+        :param content: Content of the tweet.
+        :param time: Age of the tweet.
+        :param retweets: Amount of retweets.
+        """
+        self.user = user
+        self.content = content
+        self.time = time
+        self.retweets = retweets
+
+
+def find_fastest_growing(tweets: list) -> Tweet:
+    """
+    Find the fastest growing tweet.
+
+    A tweet is the faster growing tweet if its "retweets/time" is bigger than the other's.
+    >Tweet1 is 32.5 hours old and has 64 retweets.
+    >Tweet2 is 3.12 hours old and has 30 retweets.
+    >64/32.5 is smaller than 30/3.12 -> tweet2 is the faster growing tweet.
+
+    :param tweets: Input list of tweets.
+    :return: Fastest growing tweet.
+    """
+    fastest_tweet=None
+    fastest=0
+    for tweet in tweets:
+        spreads_fastest_in_life=tweet.retweets/tweet.time
+        if spreads_fastest_in_life > fastest:
+            fastest=spreads_fastest_in_life 
+            fastest_tweet=tweet
+    return fastest_tweet
+
+def sort_by_popularity(tweets: list) -> list:
+    """
+    Sort tweets by popularity.
+
+    Tweets must be sorted in descending order.
+    A tweet is more popular than the other if it has more retweets.
+    If the retweets are even, the newer tweet is the more popular one.
+    >Tweet1 has 10 retweets.
+    >Tweet2 has 30 retweets.
+    >30 is bigger than 10 -> tweet2 is the more popular one.
+
+    :param tweets: Input list of tweets.
+    :return: List of tweets by popularity
+    """
+    def popularity_key(tweet):
+        return -tweet.retweets, tweet.time
+    sort_tweet = sorted(tweets, key=popularity_key)
+    return sort_tweet
+    
+
+
+def filter_by_hashtag(tweets: list, hashtag: str) -> list:
+    """
+    Filter tweets by hashtag.
+
+    Return a list of all tweets that contain given hashtag.
+
+    :param tweets: Input list of tweets.
+    :param hashtag: Hashtag to filter by.
+    :return: Filtered list of tweets.
+    """
+    filter_tweets=list(filter(lambda tweet: hashtag in tweet.content, tweets))
+    return filter_tweets
+
+
+def sort_hashtags_by_popularity(tweets: list) -> list:
+    """
+    Sort hashtags by popularity.
+
+    Hashtags must be sorted in descending order.
+    A hashtag's popularity is the sum of its tweets' retweets.
+    If two hashtags are equally popular, sort by alphabet from A-Z to a-z (upper case before lower case).
+    >Tweet1 has 21 retweets and has common hashtag.
+    >Tweet2 has 19 retweets and has common hashtag.
+    >The popularity of that hashtag is 19 + 21 = 40.
+
+    :param tweets: Input list of tweets.
+    :return: List of hashtags by popularity.
+    """
+    hashtag_popularity = {}
+    for tweet in tweets:
+        words = tweet.content.split()
+        for word in words:
+            if word.startswith("#"):
+                if word in hashtag_popularity:
+                    hashtag_popularity[word] += tweet.retweets
+                else:
+                    hashtag_popularity[word] = tweet.retweets
+
+    sorted_hashtag = sorted(hashtag_popularity.items(), key=lambda item: (-item[1], item[0]))
+
+    return [hashtag for hashtag, _ in sorted_hashtag]
+    
+
+
+if __name__ == '__main__':
+    tweet1 = Tweet("@realDonaldTrump", "Despite the negative press covfefe #bigsmart", 1249, 54303)
+    tweet2 = Tweet("@elonmusk", "Technically, alcohol is a solution #bigsmart", 366.4, 166500)
+    tweet3 = Tweet("@CIA", "We can neither confirm nor deny that this is our first tweet. #heart", 2192, 284200)
+    tweets = [tweet1, tweet2, tweet3]
+
+    print(find_fastest_growing(tweets).user)  # -> "@elonmusk"
+
+    filtered_by_popularity = sort_by_popularity(tweets)
+    print(filtered_by_popularity[0].user)  # -> "@CIA"
+    print(filtered_by_popularity[1].user)  # -> "@elonmusk"
+    print(filtered_by_popularity[2].user)  # -> "@realDonaldTrump"
+
+    filtered_by_hashtag = filter_by_hashtag(tweets, "#bigsmart")
+    print(filtered_by_hashtag[0].user)  # -> "@realDonaldTrump"
+    print(filtered_by_hashtag[1].user)  # -> "@elonMusk"
+
+    sorted_hashtags = sort_hashtags_by_popularity(tweets)
+    print(sorted_hashtags[0])  # -> "#heart"
+
